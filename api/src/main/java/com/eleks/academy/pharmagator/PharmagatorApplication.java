cd0b93eb87ab40b19ed6bf4d590cb44d8543797e
sergvasylchak <hustle3942@gmail.com>
2021-10-05 15:17:06 +0300

    Initial project skeleton

---
+++ api/src/main/java/com/eleks/academy/pharmagator/PharmagatorApplication.java
@@ -0,0 +1,16 @@
+package com.eleks.academy.pharmagator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+public class PharmagatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmagatorApplication.class, args);
+	}
+
+}
