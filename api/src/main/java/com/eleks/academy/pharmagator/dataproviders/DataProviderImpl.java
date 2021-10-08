448f159fefd5bbd47fe1cea93757c1e736f89a42
Pavlo Sloboda <pavlo.sloboda@eleks.com>
2021-10-08 09:54:05 +0300

    Add mocked data provider

---
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/DataProviderImpl.java
@@ -0,0 +1,25 @@
+package com.eleks.academy.pharmagator.dataproviders;
+
+import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@Service
+public class DataProviderImpl implements DataProvider {
+    @Override
+    public Stream<MedicineDto> loadData() {
+        return IntStream.rangeClosed(1, 100)
+                .mapToObj(this::buildDto);
+    }
+
+    private MedicineDto buildDto(int i) {
+        return MedicineDto.builder()
+                .externalId(String.valueOf(i))
+                .title("title" + i)
+                .price(BigDecimal.valueOf(Math.random()))
+                .build();
+    }
+}
