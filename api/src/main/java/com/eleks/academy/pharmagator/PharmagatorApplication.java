790f84aec204a69bd09ecf0c1d07df26dfbbb3ab
zyoneal <tigrestwo@ukr.net>
2021-10-11 00:48:36 +0300

    add scheduling

--- api/src/main/java/com/eleks/academy/pharmagator/PharmagatorApplication.java
+++ api/src/main/java/com/eleks/academy/pharmagator/PharmagatorApplication.java
@@ -1,16 +1,16 @@
 package com.eleks.academy.pharmagator;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
-public class PharmaApplication {
+//@EnableScheduling
+public class PharmagatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PharmaApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(PharmagatorApplication.class, args);
+    }
 
 }
