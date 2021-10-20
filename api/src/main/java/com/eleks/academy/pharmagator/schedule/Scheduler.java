1fba270aec265e77f0bd10b0cbcec887e9ab077f
zyoneal <tigrestwo@ukr.net>
2021-10-20 19:58:44 +0300

    update scheduler

--- api/src/main/java/com/eleks/academy/pharmagator/schedule/Scheduler.java
+++ api/src/main/java/com/eleks/academy/pharmagator/schedule/Scheduler.java
@@ -5,48 +5,37 @@ import com.eleks.academy.pharmagator.dao.MedicineRepository;
 import com.eleks.academy.pharmagator.dao.PriceRepository;
 import com.eleks.academy.pharmagator.dataproviders.DataProvider;
 import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
-import com.eleks.academy.pharmagator.dataproviders.dtoservices.MappingService;
-import com.eleks.academy.pharmagator.entities.Medicine;
-import com.eleks.academy.pharmagator.entities.Price;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
+@RequiredArgsConstructor
 public class Scheduler {
 
-    private final DataProvider dataProvider;
+    private final List<DataProvider> dataProviderList;
 
-    @Autowired
-    MedicineRepository medicineRepository;
+    private MedicineRepository medicineRepository;
 
-    @Autowired
-    PriceRepository priceRepository;
+    private PriceRepository priceRepository;
 
     @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
     public void schedule() {
         log.info("Scheduler started at {}", Instant.now());
-        dataProvider.loadData().forEach(this::storeToDataBase);
+        this.dataProviderList.stream().flatMap(DataProvider::loadData).forEach(this::storeToDatabase);
     }
 
+    private void storeToDatabase(MedicineDto medicineDto) {
+        log.info(medicineDto.getTitle() + " - " + medicineDto.getPrice());
 
-    private void storeToDataBase(MedicineDto medicineDto) {
 
-        Medicine medicine = new Medicine();
-        Price price = new Price();
-
-        MappingService.fromDtoToEntity(medicineDto, medicine);
-        MappingService.fromDtoToEntity(medicineDto, price);
-
-        medicineRepository.save(medicine);
-        priceRepository.save(price);
-
+        //TODO store to a data base
     }
+
 }
