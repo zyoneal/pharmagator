f666cb79e9d295762283f8538534ce44c675f1ca
zyoneal <tigrestwo@ukr.net>
2021-10-20 20:00:45 +0300

    update App

--- api/src/main/java/com/eleks/academy/pharmagator/PharmagatorApplication.java
+++ api/src/main/java/com/eleks/academy/pharmagator/PharmagatorApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class PharmagatorApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(PharmagatorApplication.class, args);
     }
 
