5ec5d65cc8adabcce202729315b99ad68152c9dd
zyoneal <tigrestwo@ukr.net>
2021-10-20 19:59:42 +0300

    add config

---
+++ api/src/main/java/com/eleks/academy/pharmagator/config/DataProvidersConfig.java
@@ -0,0 +1,37 @@
+package com.eleks.academy.pharmagator.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class DataProvidersConfig {
+
+    @Value("${pharmagator.data-providers.apteka-ds.url}")
+    private String pharmacyDSBaseUrl;
+
+    @Value("${pharmagator.data-providers.apteka-liki24.url}")
+    private String pharmacyLiki24BaseUrl;
+
+    @Bean(name = "pharmacyDSWebClient")
+    public WebClient pharmacyDSWebClient() {
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .baseUrl(pharmacyDSBaseUrl)
+                .build();
+    }
+
+    @Bean(name = "pharmacyLiki24WebClient")
+    public WebClient pharmacyLiki24WebClient() {
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .baseUrl(pharmacyLiki24BaseUrl)
+                .build();
+    }
+
+}
