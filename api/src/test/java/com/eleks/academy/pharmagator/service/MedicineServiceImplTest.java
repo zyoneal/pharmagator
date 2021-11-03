746daf25a83540a3809728654024fc5c3161d62a
Victor Piven <tigrestwo@ukr.net>
2021-11-03 12:39:57 +0200

    add unit tests for MedicineServiceImpl

---
+++ api/src/test/java/com/eleks/academy/pharmagator/service/MedicineServiceImplTest.java
@@ -0,0 +1,119 @@
+package com.eleks.academy.pharmagator.service;
+
+import com.eleks.academy.pharmagator.dataproviders.dto.input.MedicineDto;
+import com.eleks.academy.pharmagator.entities.Medicine;
+import com.eleks.academy.pharmagator.repositories.MedicineRepository;
+import com.eleks.academy.pharmagator.services.MedicineServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MedicineServiceImplTest {
+
+    @Mock
+    private MedicineRepository medicineRepository;
+
+    @InjectMocks
+    private MedicineServiceImpl medicineService;
+
+    private ModelMapper modelMapper;
+
+    private Medicine testMedicine;
+
+    private MedicineDto testMedicineDto;
+
+    @BeforeEach
+    public void init() {
+        this.modelMapper = new ModelMapper();
+        this.medicineService.setMapper(this.modelMapper);
+        this.testMedicine = new Medicine(20211103L,"title");
+        this.testMedicineDto = new MedicineDto();
+        this.testMedicineDto.setTitle("testDtoTitle");
+    }
+
+    @Test
+    public void test_findAll_ok() {
+        when(this.medicineRepository.findAll())
+                .thenReturn(LongStream.range(1L, 100L)
+                .mapToObj(i -> new Medicine(i, String.format("title %d", i)))
+                .collect(Collectors.toList())
+        );
+        final var medicines = this.medicineService.findAll();
+        assertThat(medicines).isNotEmpty().matches(list -> list.size() == 99);
+    }
+
+    @Test
+    public void test_repoReturnsEmptyList_ok() {
+
+        when(this.medicineRepository.findAll())
+                .thenReturn(List.of());
+
+        final var medicines = this.medicineService.findAll();
+        assertThat(medicines).isEmpty();
+    }
+
+    @Test
+    public void test_findById_ok() {
+        final Long id = 5L;
+        when(this.medicineRepository.findById(id))
+                .thenReturn(Optional.of(new Medicine(id, "")));
+        final var medicine = this.medicineService.findById(id);
+        assertThat(medicine).isNotEmpty();
+        assertEquals(id, medicine.get().getId());
+    }
+
+    @Test
+    public void test_findByIdOptionalEmpty_empty() {
+        final Long id = 5L;
+        when(this.medicineRepository.findById(id))
+                .thenReturn(Optional.empty());
+        final var medicine = this.medicineService.findById(id);
+        assertThat(medicine).isEmpty();
+    }
+
+    @Test
+    public void test_deleteById_ok() {
+        this.medicineService.delete(this.testMedicine.getId());
+        verify(this.medicineRepository, times(1)).deleteById(this.testMedicine.getId());
+    }
+
+    @Test
+    public void test_deleteById_bad() {
+        this.medicineService.delete(anyLong());
+        verify(this.medicineRepository, times(0)).deleteById(this.testMedicine.getId());
+    }
+
+    @Test
+    public void test_save_ok() {
+        Medicine medicine = this.modelMapper.map(this.testMedicineDto, Medicine.class);
+        when(this.medicineRepository.save(medicine)).thenReturn(medicine);
+        this.medicineService.save(this.testMedicineDto);
+        verify(this.medicineRepository, times(1)).save(Mockito.any(Medicine.class));
+    }
+
+    @Test
+    public void test_update_ok() {
+        when(medicineRepository.findById(Mockito.anyLong())).thenReturn(Optional.ofNullable(testMedicine));
+        when(medicineRepository.save(Mockito.any(Medicine.class))).thenReturn(testMedicine);
+        MedicineDto dto = this.modelMapper.map(testMedicine, MedicineDto.class);
+        final var medicine = medicineService.update(Mockito.anyLong(), dto);
+        verify(medicineRepository, times(1)).findById(Mockito.anyLong());
+        verify(medicineRepository, times(1)).save(Mockito.any(Medicine.class));
+    }
+
+}
