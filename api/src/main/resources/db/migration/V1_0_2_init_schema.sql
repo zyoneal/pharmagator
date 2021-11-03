f294fa9308fa29e3f266b5bd8a093f6dbe4951ec
Victor Piven <tigrestwo@ukr.net>
2021-11-03 21:31:33 +0200

    fix the data type in two tables

--- api/src/main/resources/db/migration/V1_0_2_init_schema.sql
+++ api/src/main/resources/db/migration/V1_0_2_init_schema.sql
@@ -1,7 +1,5 @@
 ALTER TABLE pharmacies
-ALTER COLUMN name SET DATA TYPE varchar(255);
-
-ALTER TABLE pharmacies
+ALTER COLUMN name SET DATA TYPE varchar(255),
 ALTER COLUMN medicine_link_template SET DATA TYPE varchar(255);
 
 ALTER TABLE medicines
