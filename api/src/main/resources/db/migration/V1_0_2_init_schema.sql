16d2d4319681283c30c0d79096f5f1a32c410b1e
Victor Piven <tigrestwo@ukr.net>
2021-11-03 17:55:38 +0200

    fix data type

---
+++ api/src/main/resources/db/migration/V1_0_2_init_schema.sql
@@ -0,0 +1,8 @@
+ALTER TABLE pharmacies
+ALTER COLUMN name SET DATA TYPE varchar(255);
+
+ALTER TABLE pharmacies
+ALTER COLUMN medicine_link_template SET DATA TYPE varchar(255);
+
+ALTER TABLE medicines
+ALTER COLUMN title SET DATA TYPE varchar(255);
