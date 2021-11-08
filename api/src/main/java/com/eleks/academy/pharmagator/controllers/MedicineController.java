46b6f0b23c043ee10aae0afcd69a811bfe9c6219
MarianaKh <84643950+MariamnaKh@users.noreply.github.com>
2021-11-08 23:39:08 +0200

    Change controllers return type from entity to dto (#58)
    * change return type from entity to dto
    
    * change jsonPath expression from id to name
    
    * remove blank lines
    
    * move stream method to a new line

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/MedicineController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/MedicineController.java
@@ -1,14 +1,15 @@
 package com.eleks.academy.pharmagator.controllers;
 
 import com.eleks.academy.pharmagator.dataproviders.dto.input.MedicineDto;
-import com.eleks.academy.pharmagator.entities.Medicine;
 import com.eleks.academy.pharmagator.services.MedicineService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/medicines")
@@ -16,37 +17,40 @@ import java.util.List;
 public class MedicineController {
 
     private final MedicineService medicineService;
+    private final ModelMapper modelMapper;
 
     @GetMapping
-    public List<Medicine> getAll() {
-        return this.medicineService.findAll();
+    public List<MedicineDto> getAll() {
+        return medicineService.findAll().stream()
+                .map(medicine -> modelMapper.map(medicine, MedicineDto.class))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id:[\\d]+}")
-    public ResponseEntity<Medicine> getById(@PathVariable Long id) {
-        return this.medicineService.findById(id)
-                .map(ResponseEntity::ok)
+    public ResponseEntity<MedicineDto> getById(@PathVariable Long id) {
+        return medicineService.findById(id)
+                .map(medicine -> ResponseEntity.ok(modelMapper.map(medicine, MedicineDto.class)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Medicine create(@Valid @RequestBody MedicineDto medicineDto) {
-        return this.medicineService.save(medicineDto);
+    public MedicineDto create(@Valid @RequestBody MedicineDto medicineDto) {
+        return modelMapper.map(medicineService.save(medicineDto), MedicineDto.class);
     }
 
     @PutMapping("/{id:[\\d]+}")
-    public ResponseEntity<Medicine> update(
+    public ResponseEntity<MedicineDto> update(
             @PathVariable Long id,
             @Valid @RequestBody MedicineDto medicineDto) {
 
-        return this.medicineService.update(id, medicineDto)
-                .map(ResponseEntity::ok)
+        return medicineService.update(id, medicineDto)
+                .map(medicine -> ResponseEntity.ok(modelMapper.map(medicine, MedicineDto.class)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id:[\\d]+}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
-        this.medicineService.delete(id);
+        medicineService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
--- api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
@@ -1,14 +1,15 @@
 package com.eleks.academy.pharmagator.controllers;
 
 import com.eleks.academy.pharmagator.dataproviders.dto.input.PharmacyDto;
-import com.eleks.academy.pharmagator.entities.Pharmacy;
 import com.eleks.academy.pharmagator.services.PharmacyService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,38 +17,40 @@ import java.util.List;
 public class PharmacyController {
 
     private final PharmacyService pharmacyService;
+    private final ModelMapper modelMapper;
 
     @GetMapping
-    public List<Pharmacy> getAll() {
-        return this.pharmacyService.findAll();
+    public List<PharmacyDto> getAll() {
+        return pharmacyService.findAll().stream()
+                .map(pharmacy -> modelMapper.map(pharmacy, PharmacyDto.class))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id:[\\d]+}")
-    public ResponseEntity<Pharmacy> getById(@PathVariable Long id) {
-
-        return this.pharmacyService.findById(id)
-                .map(ResponseEntity::ok)
+    public ResponseEntity<PharmacyDto> getById(@PathVariable Long id) {
+        return pharmacyService.findById(id)
+                .map(pharmacy -> ResponseEntity.ok(modelMapper.map(pharmacy, PharmacyDto.class)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Pharmacy> create(@Valid @RequestBody PharmacyDto pharmacyDto) {
-        return ResponseEntity.ok(this.pharmacyService.save(pharmacyDto));
+    public PharmacyDto create(@Valid @RequestBody PharmacyDto pharmacyDto) {
+        return modelMapper.map(pharmacyService.save(pharmacyDto), PharmacyDto.class);
     }
 
     @PutMapping("/{id:[\\d]+}")
-    public ResponseEntity<Pharmacy> update(
+    public ResponseEntity<PharmacyDto> update(
             @PathVariable Long id,
             @Valid @RequestBody PharmacyDto pharmacyDto) {
 
-        return this.pharmacyService.update(id, pharmacyDto)
-                .map(ResponseEntity::ok)
+        return pharmacyService.update(id, pharmacyDto)
+                .map(pharmacy -> ResponseEntity.ok(modelMapper.map(pharmacy, PharmacyDto.class)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id:[\\d]+}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
-        this.pharmacyService.deleteById(id);
+        pharmacyService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
