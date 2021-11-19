f92e1393059a2961938c0aebfb24e368afe597a2
MuchosChuchos <88827865+MuchosChuchos@users.noreply.github.com>
2021-11-19 19:34:59 +0300

    Add validation to request body in controllers (#88)
    * fix: Add validation to request body in controllers
    
    Closes #86
    
    * refactor: Change @notnull to @notblank
    
    * refactor: Set @NotNull for price field.
    
    * refactor(Add @NoArgs... and @AllArgs... to dtos):

--- api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/input/PriceDto.java
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/input/PriceDto.java
@@ -1,16 +1,24 @@
 package com.eleks.academy.pharmagator.dataproviders.dto.input;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PriceDto {
 
     @Min(value = 0)
+    @NotNull
     private BigDecimal price;
 
+    @NotBlank
     private String externalId;
 
 }
