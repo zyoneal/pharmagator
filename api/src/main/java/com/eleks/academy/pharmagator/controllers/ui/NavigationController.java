df34b6b54d1d35f53b637a452b2f2b73a17d1489
Victor Piven <tigrestwo@ukr.net>
2021-12-03 00:01:44 +0200

    add input medicine

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/ui/NavigationController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/ui/NavigationController.java
@@ -1,13 +1,17 @@
 package com.eleks.academy.pharmagator.controllers.ui;
 
+import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
 import com.eleks.academy.pharmagator.entities.Pharmacy;
 import com.eleks.academy.pharmagator.services.ExportService;
+import com.eleks.academy.pharmagator.services.ImportService;
 import com.eleks.academy.pharmagator.services.MedicineService;
 import com.eleks.academy.pharmagator.services.PharmacyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,6 +24,7 @@ public class NavigationController {
     private final MedicineService medicineService;
     private final PharmacyService pharmacyService;
     private final ExportService exportService;
+    private final ImportService importService;
 
     @GetMapping({"", "/", "/index", "/index.html", "/home", "/homepage"})
     public String getHomePage() {
@@ -39,6 +44,18 @@ public class NavigationController {
         model.addAttribute("pricesMap", exportService.getMapPricesFromDatabase());
         model.addAttribute("pharmacies", pharmacies);
         return "price";
+    }
+
+    @GetMapping("/addmedicine")
+    public String addMedicine(Model model) {
+       model.addAttribute("med",new MedicineDto());
+        return "add-medicine";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("med") MedicineDto medicineDto){
+        importService.storeToDatabase(medicineDto);
+        return "redirect:/ui";
     }
 
 }
