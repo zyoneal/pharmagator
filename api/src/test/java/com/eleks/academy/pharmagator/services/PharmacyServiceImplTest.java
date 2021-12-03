2a9ef77b0666f7574ab1c31d90a6118d96591d69
Victor Piven <tigrestwo@ukr.net>
2021-12-03 12:21:17 +0200

    add tests for PharmacyServiceImpl

--- api/src/test/java/com/eleks/academy/pharmagator/services/PharmacyServiceImplTest.java
+++ api/src/test/java/com/eleks/academy/pharmagator/services/PharmacyServiceImplTest.java
@@ -1,17 +1,29 @@
 package com.eleks.academy.pharmagator.services;
 
+import com.eleks.academy.pharmagator.dataproviders.dto.input.MedicineDto;
+import com.eleks.academy.pharmagator.dataproviders.dto.input.PharmacyDto;
+import com.eleks.academy.pharmagator.entities.Medicine;
 import com.eleks.academy.pharmagator.entities.Pharmacy;
 import com.eleks.academy.pharmagator.repositories.PharmacyRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doThrow;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -19,6 +31,75 @@ class PharmacyServiceImplTest {
 
     @Mock
     private PharmacyRepository repository;
+
+    @InjectMocks
+    PharmacyServiceImpl service;
+
+    PharmacyDto pharmacyDto;
+
+    List<Pharmacy> pharmacyList;
+
+    @BeforeEach
+    void init() {
+        pharmacyDto = new PharmacyDto();
+        pharmacyDto.setName("Aptslav");
+        pharmacyDto.setMedicineLinkTemplate("aptslav.com");
+
+        pharmacyList = new ArrayList<>();
+        pharmacyList.add(new Pharmacy(1L, "DS", "ds.com"));
+        pharmacyList.add(new Pharmacy(2L, "Liki24", "liki24.com"));
+        pharmacyList.add(new Pharmacy(3L, "ANC", "anc.com"));
+    }
+
+    @Test
+    void test_findAll_ok() {
+        when(this.repository.findAll()).thenReturn(pharmacyList);
+        List<Pharmacy> pharmacies = this.service.findAll();
+        verify(repository).findAll();
+        assertThat(pharmacies).isNotEmpty().matches(list -> list.size() == pharmacyList.size());
+    }
+
+    @Test
+    void test_repoReturnsEmptyList_ok() {
+        when(this.repository.findAll()).thenReturn(List.of());
+        final var pharmacies = this.service.findAll();
+        assertThat(pharmacies).isEmpty();
+    }
+
+    @Test
+    void test_findById_ok() {
+        final var id = 10L;
+        when(this.repository.findById(id)).thenReturn(Optional.of(new Pharmacy(id, "", "")));
+        final var pharmacies = this.service.findById(id);
+        assertThat(pharmacies).isNotEmpty();
+        assertEquals(id, pharmacies.get().getId());
+    }
+
+    @Test
+    void test_finById_Empty() {
+        final var id = 10L;
+        when(this.repository.findById(id)).thenReturn(Optional.empty());
+        final var pharmacies = this.service.findById(id);
+        assertThat(pharmacies).isEmpty();
+    }
+
+    @Test
+    @Disabled
+    void canSavePharmacy() {
+        service.save(pharmacyDto);
+        ArgumentCaptor<Pharmacy> pharmacyArgumentCaptor = ArgumentCaptor.forClass(Pharmacy.class);
+        verify(repository).save(pharmacyArgumentCaptor.capture());
+    }
+
+    @Test
+    @Disabled
+    void canUpdatePharmacy() {
+        final var id = 10L;
+        service.update(id, pharmacyDto);
+        ArgumentCaptor<Pharmacy> pharmacyArgumentCaptor = ArgumentCaptor.forClass(Pharmacy.class);
+        verify(repository).findById(id);
+        verify(repository).save(pharmacyArgumentCaptor.capture());
+    }
 
     @Test
     void deleteById_ok() {
