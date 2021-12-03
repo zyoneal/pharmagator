d53c6426c0f9bca2534c3922a08e0d504762b510
Victor Piven <tigrestwo@ukr.net>
2021-12-03 11:44:17 +0200

    add some tests for MedicineServiceImpl

--- api/src/test/java/com/eleks/academy/pharmagator/services/MedicineServiceImplTest.java
+++ api/src/test/java/com/eleks/academy/pharmagator/services/MedicineServiceImplTest.java
@@ -1,17 +1,26 @@
 package com.eleks.academy.pharmagator.services;
 
+
+import com.eleks.academy.pharmagator.dataproviders.dto.input.MedicineDto;
 import com.eleks.academy.pharmagator.entities.Medicine;
 import com.eleks.academy.pharmagator.repositories.MedicineRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doThrow;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -19,6 +28,58 @@ class MedicineServiceImplTest {
 
     @Mock
     private MedicineRepository repository;
+
+    @InjectMocks
+    private MedicineServiceImpl medicineService;
+
+    MedicineDto medicineDto;
+
+    @BeforeEach
+    void init() {
+        medicineDto = new MedicineDto();
+        medicineDto.setTitle("Aspirin");
+    }
+
+    @Test
+    void canFindAllMedicines() {
+        repository.findAll();
+        verify(repository).findAll();
+    }
+
+    @Test
+    void test_findById_ok() {
+        final var id = 10L;
+        when(this.repository.findById(id)).thenReturn(Optional.of(new Medicine(id, "")));
+        final var medicine = this.medicineService.findById(id);
+        assertThat(medicine).isNotEmpty();
+        assertEquals(id, medicine.get().getId());
+    }
+
+    @Test
+    void test_finById_Empty() {
+        final var id = 10L;
+        when(this.repository.findById(id)).thenReturn(Optional.empty());
+        final var medicine = this.medicineService.findById(id);
+        assertThat(medicine).isEmpty();
+    }
+
+    @Test
+    @Disabled
+    void canSaveMedicine() {
+        medicineService.save(medicineDto);
+        ArgumentCaptor<Medicine> medicineArgumentCaptor = ArgumentCaptor.forClass(Medicine.class);
+        verify(repository).save(medicineArgumentCaptor.capture());
+    }
+
+    @Test
+    @Disabled
+    void canUpdateMedicine() {
+        final var id = 10L;
+        medicineService.update(id,medicineDto);
+        ArgumentCaptor<Medicine> medicineArgumentCaptor = ArgumentCaptor.forClass(Medicine.class);
+        verify(repository).findById(Mockito.anyLong());
+        verify(repository).save(medicineArgumentCaptor.capture());
+    }
 
     @Test
     void deleteById_ok() {
