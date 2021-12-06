135c960d482428e687a596e70e00239b51b9d4fe
MarianaKh <84643950+MariamnaKh@users.noreply.github.com>
2021-12-06 14:27:02 +0200

    Pharmacy service & medicine service tests (#141)
    * add medicine service test
    
    * add pharmacy service tests
    
    * fix code smells
    
    * add deleteById_ok & rename method
    
    * rename methods
    
    * specify exception

--- api/src/test/java/com/eleks/academy/pharmagator/services/MedicineServiceImplTest.java
+++ api/src/test/java/com/eleks/academy/pharmagator/services/MedicineServiceImplTest.java
@@ -1,17 +1,28 @@
 package com.eleks.academy.pharmagator.services;
 
+import com.eleks.academy.pharmagator.dataproviders.dto.input.MedicineDto;
 import com.eleks.academy.pharmagator.entities.Medicine;
 import com.eleks.academy.pharmagator.repositories.MedicineRepository;
+import nl.altindag.log.LogCaptor;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doThrow;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -20,12 +31,87 @@ class MedicineServiceImplTest {
     @Mock
     private MedicineRepository repository;
 
+    @InjectMocks
+    private MedicineServiceImpl medicineService;
+
+    @Mock
+    private static ModelMapper modelMapper;
+
+    private static Medicine medicine1;
+    private static Medicine medicine2;
+    private static List<Medicine> medicines;
+    private static MedicineDto medicineDto;
+
+    @BeforeAll
+    public static void setup() {
+        medicine1 = new Medicine(1L, "Ibuprofen");
+        medicine2 = new Medicine(3L, "Vitamin C");
+        medicines = Arrays.asList(medicine1, medicine2);
+        medicineDto = new MedicineDto("Ibuprofen");
+    }
+
     @Test
-    void deleteById_ok() {
+    void getAllMedicines_ok() {
+        when(repository.findAll()).thenReturn(medicines);
+
+        List<Medicine> medicineList = medicineService.findAll();
+
+        assertEquals(2, medicineList.size());
+    }
+
+    @Test
+    void saveMedicine_medicineIsCreated() {
+        when(repository.save(any())).thenReturn(medicine1);
+        when(modelMapper.map(eq(medicineDto), any())).thenReturn(medicine1);
+
+        assertEquals(medicine1, medicineService.save(medicineDto));
+
+        verify(repository, times(1)).save(any(Medicine.class));
+    }
+
+    @Test
+    void findMedicineById_shouldReturnRespectiveMedicine() {
+        when(repository.findById(anyLong())).thenReturn(Optional.of(medicine1));
+
+        Medicine medicine2 = medicineService.findById(medicine1.getId()).get();
+
+        verify(repository, times(1)).findById(anyLong());
+
+        assertEquals(medicine1.getId(), medicine2.getId());
+        assertEquals(medicine1.getTitle(), medicine2.getTitle());
+    }
+
+    @Test
+    void updateMedicineById_medicineIsUpdated() {
+        when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(medicine1));
+        when(repository.save(any(Medicine.class))).thenReturn(medicine1);
+        when(modelMapper.map(eq(medicineDto), any())).thenReturn(medicine1);
+
+        assertEquals(medicine1, medicineService.update(anyLong(), medicineDto).get());
+    }
+
+    @Test
+    void deleteById_isOk() {
+        doNothing().when(repository).deleteById(anyLong());
+
+        medicineService.deleteById(medicine1.getId());
+
+        verify(repository, times(1)).deleteById(medicine1.getId());
+    }
+
+    @Test
+    void deleteById_nonExistingId_throwsEmptyResultDataAccessException() {
         Long wrongMedicineId = 1000002L;
         String exceptionMessage = String.format("No class %s entity with id %d exists!", Medicine.class.getCanonicalName(), wrongMedicineId);
 
+        LogCaptor logCaptor = LogCaptor.forClass(MedicineServiceImpl.class);
+        logCaptor.setLogLevelToInfo();
+
         doThrow(new EmptyResultDataAccessException(exceptionMessage, 0)).when(repository).deleteById(wrongMedicineId);
+
+        medicineService.deleteById(wrongMedicineId);
+
+        assertThat(logCaptor.getInfoLogs()).containsExactly(exceptionMessage);
 
         Exception exception = assertThrows(EmptyResultDataAccessException.class, () -> {
             repository.deleteById(wrongMedicineId);
--- api/src/test/java/com/eleks/academy/pharmagator/services/PharmacyServiceImplTest.java
+++ api/src/test/java/com/eleks/academy/pharmagator/services/PharmacyServiceImplTest.java
@@ -1,17 +1,27 @@
 package com.eleks.academy.pharmagator.services;
 
+import com.eleks.academy.pharmagator.dataproviders.dto.input.PharmacyDto;
 import com.eleks.academy.pharmagator.entities.Pharmacy;
 import com.eleks.academy.pharmagator.repositories.PharmacyRepository;
+import nl.altindag.log.LogCaptor;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doThrow;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -20,12 +30,87 @@ class PharmacyServiceImplTest {
     @Mock
     private PharmacyRepository repository;
 
+    @InjectMocks
+    private PharmacyServiceImpl pharmacyService;
+
+    @Mock
+    private static ModelMapper modelMapper;
+
+    private static Pharmacy pharmacy1;
+    private static Pharmacy pharmacy2;
+    private static List<Pharmacy> pharmacyList;
+    private static PharmacyDto pharmacyDto;
+
+    @BeforeAll
+    public static void setup() {
+        pharmacy1 = new Pharmacy(8L, "My pharma", "linkk.com");
+        pharmacy2 = new Pharmacy(9L, "Second pharma", "secongPharma.com");
+        pharmacyDto = new PharmacyDto("My pharma", "linkk.com");
+        pharmacyList = Arrays.asList(pharmacy1, pharmacy2);
+    }
+
     @Test
-    void deleteById_ok() {
+    void getAllPharmacies_isOk() {
+        when(repository.findAll()).thenReturn(pharmacyList);
+
+        List<Pharmacy> pharmacyList = pharmacyService.findAll();
+
+        assertEquals(2, pharmacyList.size());
+    }
+
+    @Test
+    void savePharmacy_pharmacyIsCreated() {
+        when(repository.save(any())).thenReturn(pharmacy1);
+        when(modelMapper.map(eq(pharmacyDto), any())).thenReturn(pharmacy1);
+
+        assertEquals(pharmacy1, pharmacyService.save(pharmacyDto));
+
+        verify(repository, times(1)).save(any());
+    }
+
+    @Test
+    void findPharmacyById_shouldReturnRespectivePharmacy() {
+        when(repository.findById(anyLong())).thenReturn(Optional.of(pharmacy1));
+
+        Pharmacy pharmacy = pharmacyService.findById(pharmacy1.getId()).get();
+
+        verify(repository, times(1)).findById(anyLong());
+
+        assertEquals(pharmacy1.getName(), pharmacy.getName());
+        assertEquals(pharmacy1.getMedicineLinkTemplate(), pharmacy.getMedicineLinkTemplate());
+    }
+
+    @Test
+    void updatePharmacyById_pharmacyIsUpdated() {
+        when(modelMapper.map(eq(pharmacyDto), any())).thenReturn(pharmacy1);
+        when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(pharmacy1));
+        when(repository.save(any(Pharmacy.class))).thenReturn(pharmacy1);
+
+        assertEquals(pharmacy1, pharmacyService.update(anyLong(), pharmacyDto).get());
+    }
+
+    @Test
+    void deleteById_isOk() {
+        doNothing().when(repository).deleteById(anyLong());
+
+        pharmacyService.deleteById(pharmacy1.getId());
+
+        verify(repository, times(1)).deleteById(pharmacy1.getId());
+    }
+
+    @Test
+    void deleteById_nonExistingId_throwsEmptyResultDataAccessException() {
         Long wrongPharmacyId = 1000002L;
         String exceptionMessage = String.format("No class %s entity with id %d exists!", Pharmacy.class.getCanonicalName(), wrongPharmacyId);
 
+        LogCaptor logCaptor = LogCaptor.forClass(PharmacyServiceImpl.class);
+        logCaptor.setLogLevelToInfo();
+
         doThrow(new EmptyResultDataAccessException(exceptionMessage, 0)).when(repository).deleteById(wrongPharmacyId);
+
+        pharmacyService.deleteById(wrongPharmacyId);
+
+        assertThat(logCaptor.getInfoLogs()).containsExactly(exceptionMessage);
 
         Exception exception = assertThrows(EmptyResultDataAccessException.class, () -> {
             repository.deleteById(wrongPharmacyId);
